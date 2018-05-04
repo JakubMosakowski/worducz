2fbb3b39cc3978066756daf89d7b3fbba29e3fde
JakubMosakowski <mosakowskijakubsm@gmail.com>
2018-05-04 11:24:34 +0200

    Test2 of remote server

--- frontend/views/jezyk/index.php
+++ frontend/views/jezyk/index.php
@@ -18,6 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a('Stwórz język', ['create'], ['class' => 'btn btn-success']) ?>
         Test echo
+        Test echo2
     </p>
 
     <?= GridView::widget([
